d4fbc745b18362d0b62e14f2361600a6a27f369f
Nancy <naspirault@gmail.com>
2022-04-29 13:22:54 -0400

    Add files via upload
    Deuxième commit

---
+++ Pizza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPizza
+{
+    internal class Pizza
+    {
+        public string name { get; private set; }
+        public float price { get; private set; }
+        public bool vegetarian { get; private set; }
+        List<string> ingredients { get; set; }
+        public Pizza(string aName, float aPrice, bool isVeg, List<string> listIngredients)
+        {
+            name = aName; 
+            price = aPrice; 
+            vegetarian = isVeg;
+            ingredients = listIngredients;
+        }
+        public override string ToString()
+        {
+            string txt = "";
+            
+            txt += FormatFirstLetterUppercase(name);
+            string badgeVeg = vegetarian ? " (V)" : "";  //nouveau
+            //if (vegetarian) { txt += "(V)"; }
+            txt += badgeVeg + " - " + price + "$\n";
+            txt += string.Join(", ", ingredients) + "\n"; //nouveau
+            return txt;
+        }
+        private static string FormatFirstLetterUppercase(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+                return s;
+            string lowercase = s.ToLower();
+            string uppercase = s.ToUpper();
+            string result = uppercase[0] + lowercase[1..] ;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+    - Class Pizza
+    - nom (4 fromages)
+    - prix : 11,5
+    - vegetarienne (vrai ou faux)
+    - constructeur
+    - afficher
+    - 4 framges (V) - 11,5$
+    - créer une liste de pizzas
+    - boucler pour afficher les pizzas.
+*/
+
+using ProjetPizza;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var listPizzas = new List<Pizza>() {
+    new Pizza("4 fromages", 11.50f, true, new List<string> {"cantal", "mozzarella","gruyère","conté"}),
+    new Pizza("Indienne", 10.5f, true, new List<string> {"curry","mozzarella","poulet","poivron","oignon","coriandre"}),
+    new Pizza("Mexicaine", 13f, false, new List<string> {"boeuf","mozzarella","mais","tomates","oignon","coriandre"}),
+    new Pizza("margherita", 8f, false, new List<string>{"sauce tomate","mozzarella","basilic"}),
+    new Pizza("Calzone", 12f, false, new List<string>{"tomate","jambon","persil", "oignons"}),
+    new Pizza("Complète", 9.50f, false,new List<string>{"jambon","oeuf","fromage"})
+};
+
+Pizza pizzaPriceMin = null;
+Pizza pizzaPriceMax = null;
+
+pizzaPriceMin = listPizzas[0];
+pizzaPriceMax = listPizzas[0];
+
+foreach(var onePizza in listPizzas)
+{
+    if (onePizza.price < pizzaPriceMin.price)
+    {
+        pizzaPriceMin = onePizza;
+    }
+    if (onePizza.price > pizzaPriceMax.price)
+    {
+        pizzaPriceMax = onePizza;
+    }
+}
+//La pizza la moins cher est ?? et la pizza la plus cher est ??
+Console.WriteLine($"La pizza la moins cher est: {pizzaPriceMin} \nLa pizza la plus cher est: {pizzaPriceMax}");
+foreach (var onePizza in listPizzas)
+{
+    Console.WriteLine(onePizza);
+}
